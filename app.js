bc591a606808850266af460c9f4197649a10852c
sudheerdarla <sudheer.darla11@gmail.com>
2016-11-23 22:53:46 +0530

    added modal

--- app.js
+++ app.js
@@ -11,9 +11,34 @@ $(document).ready(function() {
 		.addClass('is-liked');
 	});
 
+	//----------------------------------
+	//		ADD LINK
+	//----------------------------------
+
 	$('.js-add-link').on('click', function(event) {
 		event.preventDefault();
 
 	$('.js-form').toggleClass('is-visible');
 	});
+
+	//----------------------------------
+	//		MODAL
+	//----------------------------------
+
+	$('.js-show-modal').on('click', function(event) {
+		event.preventDefault();
+
+		$('.js-modal').addClass('is-visible');
+		$('.js-modal-overlay').addClass('is-visible');
+
+		$('.js-modal-overlay').on('click', function(event) {
+			event.preventDefault();
+
+			$('.js-modal, .js-modal-overlay').removeClass('is-visible');
+		})
+	});
 });
+
+
+
+
